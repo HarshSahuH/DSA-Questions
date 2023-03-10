9adeeb958e300224f5a40d73cfe5d02e08444b81
Harsh Sahu <121496378+HarshSahuH@users.noreply.github.com>
2023-03-10 16:38:44 +0530

    Added solution - LeetHub

---
+++ Factorial - GFG/factorial.cpp
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    long long int factorial(int N){
+        
+        if(N==1 ||N==0){
+            return 1;
+        }
+        
+        long long int ans =1;
+        ans = N*factorial(N-1);
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.factorial(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends

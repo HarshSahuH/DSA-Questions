762dbafa1692a704558851b16bbe60dc03d7c8e5
Harsh Sahu <121496378+HarshSahuH@users.noreply.github.com>
2023-05-31 06:50:39 +0530

    Time: 702 ms (18.26%), Space: 67.1 MB (15.77%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-       
+        //edge case
         if(nums.size()==0){
             return 0;
         }
-        unordered_set<int> s;
+        
+        unordered_set<int> s; //insertion and find will happen in O(1)
         int ans = INT_MIN;
         
+        //putting all element into set 
         for(int i=0; i<nums.size(); i++){
            s.insert(nums[i]); 
         }
         
+        //now traverse the array and if nums[i] - 1 is present in set 
+        // then no need to check for further lesser than that element because 
+        // that element will never.
+        // check for higher one 
         for(int i=0; i<nums.size(); i++){
             
             if(s.find(nums[i]-1) != s.end()){
                 continue;
             }
             else{
+                //set mein nahi mila nums[i]-1 , iska mtlb this may be starting of                          consecutive sequence
                 int count = 0;
                 int curr = nums[i];
                 while(s.find(curr) != s.end()){

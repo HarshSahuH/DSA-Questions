232c98eac79d49f96caf230188e4a0e07f697349
Harsh Sahu <121496378+HarshSahuH@users.noreply.github.com>
2023-06-12 12:40:55 +0530

    Time: 4 ms (34.40%), Space: 6.1 MB (62.95%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if (s.length() != goal.length()) return false;
+        
+        if (s.empty()) return true;  // Both strings are empty, consider it as a rotation
+        
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            if (s[i] == goal[0]) {
+                int j = 0;
+                while (j < n && s[(i + j) % n] == goal[j]) {
+                    j++;
+                }
+                
+                if (j == n) return true;  // Found a rotation
+            }
+        }
+        
+        return false;  // No rotation found
+    }
+};

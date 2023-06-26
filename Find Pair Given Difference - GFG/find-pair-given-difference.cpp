f5e666dbc1b5ce7d109dedc2d7329e9a3a8e842a
Harsh Sahu <121496378+HarshSahuH@users.noreply.github.com>
2023-06-26 10:59:55 +0530

    Added solution - LeetHub

--- Find Pair Given Difference - GFG/find-pair-given-difference.cpp
+++ Find Pair Given Difference - GFG/find-pair-given-difference.cpp
@@ -29,7 +29,29 @@ int main()
 
 
 bool findPair(int arr[], int size, int n){
-    
+ //Approach 2 T.C. O( N logN + N) = NlogN , S.C. O(1)
+ sort(arr,arr+size);
+ int i = 0; 
+ int j = 1;
+ 
+ while(i < size && j < size){
+     if(arr[j] - arr[i] == n && i!=j){
+         return true;
+     }
+     else if(arr[j] - arr[i] < n){
+         j++;
+     }
+     else{
+         i++;
+     }
+ }
+ return false;
+}
+
+
+
+ /*
+ Approach 1 Brute Force T.C.O(N^2) S.C. O(1)   
     for(int i=0; i<size; i++)
     {
         for(int j=i+1; j<size; j++)
@@ -39,4 +61,4 @@ bool findPair(int arr[], int size, int n){
         }
     }
     return false;
-}
+*/

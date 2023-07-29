573ec74284c628945bff3b2bf438b3f19861fd9a
Harsh Sahu <121496378+HarshSahuH@users.noreply.github.com>
2023-07-29 10:24:21 +0530

    Time: 10 ms (95.33%), Space: 20.8 MB (9.02%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -13,11 +13,15 @@ public:
         vector<int> mxRight(n);
         vector<int> water(n);
         
+        
+         // this array will store maximum value in left of arr of that particular arr[i]
         mxLeft[0]=height[0];
         for(int i=1; i<n; i++){
-            mxLeft[i] = max(height[i],mxLeft[i-1]);
+            mxLeft[i] = max(height[i] , mxLeft[i-1]);
         }
         
+        
+         // this array will store maximum value in right of arr of that particular arr[i]
         mxRight[n-1] = height[n-1];
         for(int i=n-2; i>=0; i--){
             mxRight[i] = max(height[i] , mxRight[i+1]);
@@ -25,12 +29,13 @@ public:
         
         
         for(int i=0; i<n; i++){
-        water[i] = (min(mxRight[i] , mxLeft[i])) - height[i];     
+         water[i] = (min(mxRight[i] , mxLeft[i])) - height[i];  
+         Water_Trap_sum += water[i]; 
         }
         
-        for(int i=0; i<n; i++){
-            Water_Trap_sum += water[i];  
-        }
+       // for(int i=0; i<n; i++){
+          //  Water_Trap_sum += water[i];  
+        //}
          
    
         return Water_Trap_sum;

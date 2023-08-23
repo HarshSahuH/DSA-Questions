31b63c91ba4555a43095dc6d46a3a7f93874dc43
Harsh Sahu <121496378+HarshSahuH@users.noreply.github.com>
2023-08-23 08:55:08 +0530

    Time: 5 ms (22.20%), Space: 7.3 MB (5.49%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.cpp
+++ 0767-reorganize-string/0767-reorganize-string.cpp
@@ -1,48 +1,42 @@
 class Solution {
 public:
     string reorganizeString(string s) {
-        
-        string ans ="";
-        vector<int> charCounts(26,0);
-        for(char x:s)
-        {
-            charCounts[x-'a']++; 
+        vector<int> charCounts(26, 0);
+        for (char c : s) {
+            charCounts[c - 'a'] = charCounts[c - 'a'] + 1;
         }
-        
+
+        // Max heap ordered by character counts
         priority_queue<vector<int>> pq;
-        for(int i=0; i<26; i++)
-        {
-            if(charCounts[i] >0)
-            {
-                pq.push(vector<int>{charCounts[i],i+'a'});
+        for (int i = 0; i < 26; i++) {
+            if (charCounts[i] > 0) {
+                pq.push(vector<int>{charCounts[i], i + 'a'});
             }
         }
         
-        while(!pq.empty())
-        {
+        string result;
+        while (!pq.empty()) {
             auto first = pq.top();
             pq.pop();
-            if(ans.empty() || first[1] != ans.back())
-            {
-                ans = ans + char(first[1]);
-                if(--first[0] > 0)
-                pq.push(first);
-            }
-            else
-            {
-                if(pq.empty())
+            if (result.empty() || first[1] != result.back()) {
+                result += char(first[1]);
+                if (--first[0] > 0) {
+                    pq.push(first);
+                }
+            } else {
+                if (pq.empty()) {
                     return "";
+                }
                 auto second = pq.top();
                 pq.pop();
-                ans = ans + char(second[1]);
-                if(--second[0] > 0)
-                {
+                result += char(second[1]);
+                if (--second[0] > 0) {
                     pq.push(second);
                 }
                 pq.push(first);
             }
-            
         }
-        return ans;
+
+        return result;
     }
 };

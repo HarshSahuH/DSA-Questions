89678c65207a2a718d54cba8d43806dd6da7e710
Harsh Sahu <121496378+HarshSahuH@users.noreply.github.com>
2023-10-13 17:31:35 +0530

    Time: 3 ms (10.41%), Space: 6.7 MB (7.02%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+    int minCost(int n, vector<int>& dp){
+        if(dp[n] != -1) return dp[n];
+        if(n<=2) return n;
+        
+        return dp[n] = minCost(n-1,dp) + minCost(n-2,dp);
+    }
+public:
+    int climbStairs(int n) {
+        vector<int> dp(n+1,-1);
+        if(n<=2)return n;
+        minCost(n,dp);
+        return dp[n];
+    }
+};

eb8d8b5b2fdc08d106517b9c36234d3a59756659
Harsh Sahu <121496378+HarshSahuH@users.noreply.github.com>
2023-10-23 15:00:55 +0530

    Time: 4 ms (22.26%), Space: 6.3 MB (26.74%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        
+     //Approach 1 
+        if(n==0) return false;
+        while(n%4 == 0){
+            n/=4;
+        }
+        if(n==1)return true;
+        return false;
+    }
+};

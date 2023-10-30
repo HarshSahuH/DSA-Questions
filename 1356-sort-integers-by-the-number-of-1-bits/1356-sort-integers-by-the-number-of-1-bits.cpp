2e71955d3f242652eeb38f0acdfe156c153d6e1e
Harsh Sahu <121496378+HarshSahuH@users.noreply.github.com>
2023-10-30 17:54:48 +0530

    Time: 23 ms (6.67%), Space: 10.5 MB (53.39%) - LeetHub

---
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    static int findWeight(int num) {
+        int mask = 1;
+        int weight = 0;
+        
+        while (num > 0) {
+            if ((num & mask) > 0) {
+                weight++;
+                num ^= mask;
+            }
+            
+            mask <<= 1;
+        }
+        
+        return weight;
+    }
+     static bool compare(int a, int b) {
+        if (findWeight(a) == findWeight(b)) {
+            return a < b;
+        }
+        
+        return findWeight(a) < findWeight(b);
+    }
+    vector<int> sortByBits(vector<int>& arr) {
+        sort(arr.begin(), arr.end(), compare);
+        return arr;
+    }
+};

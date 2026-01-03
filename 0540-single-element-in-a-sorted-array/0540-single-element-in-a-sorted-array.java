784bd3fa2fa9851e1267bc3036694b8bdfc8c1c8
Harsh Sahu <121496378+HarshSahuH@users.noreply.github.com>
2026-01-03 10:11:15 +0530

    Time: 0 ms (100%), Space: 53 MB (18.66%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -3,25 +3,28 @@ class Solution {
         int n = nums.length;    
 
         if(n == 1) return nums[0];
-        if(n == 2) return nums[0];
+        if(nums[0] != nums[1]) return nums[0];
+        if(nums[n-1] != nums[n-2]) return nums[n-1];
 
+        int low = 1;
+        int high = n-2;
 
-        int s = 0;
-        int e = nums.length-1;
-        while(s<=e){
-            if(nums[s] == nums[s+1]) {
-                s = s+2;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+
+            if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]){
+                return nums[mid];
             }
+
+            else if( (mid % 2==0 && nums[mid] == nums[mid+1]) || 
+                     (mid % 2==1 && nums[mid] == nums[mid-1])  ){
+                        low = mid + 1;
+                      }
             else{
-                return nums[s];
+                high = mid -1;
             }
+        }   
 
-            if(nums[e] == nums[e-1]){
-                e = e-2;
-            }else{
-                return nums[e];
-            }
-        }
         return -1;
     }
 }

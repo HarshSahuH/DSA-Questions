a9a86a1690ac736f8aa7d4ff0bba05fa7318d539
Harsh Sahu <121496378+HarshSahuH@users.noreply.github.com>
2026-07-31 00:01:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum Platforms/minimum-platforms.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int minPlatform(int arr[], int dep[]) {
+        //  code here
+        Arrays.sort(arr);
+        Arrays.sort(dep);
+        
+        int n = arr.length;
+        int i=0; // arival time 
+        int j=0; // depature time 
+        int result = 0;
+        int platformNeeded =0;
+        
+        while(i < n && j< n){
+            if(arr[i] <= dep[j]){
+                 // If next train arrives before current one departs
+                platformNeeded++;
+                i++;
+            }
+            else{
+                // One train departs, platform freed
+                platformNeeded--;
+                j++;
+            }
+            if(platformNeeded > result){
+                result = platformNeeded;
+            }
+        }
+      return result;    
+    }
+}

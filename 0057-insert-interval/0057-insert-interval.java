eb30567808680cbaf55fefcb80afd45f8fd88756
Harsh Sahu <121496378+HarshSahuH@users.noreply.github.com>
2026-08-17 01:46:43 +0530

    Time: 1 ms (98.16%), Space: 47 MB (76.19%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,51 +1,34 @@
 class Solution {
    
     public int[][] insert(int[][] intervals, int[] newInterval) {
-       int i=0; 
-       while(i < intervals.length){
-        // Case 1: Current interval ends before newInterval starts (No overlap)
-        if(intervals[i][1] < newInterval[0]){
+
+       List<int[]> ans = new ArrayList<>(); 
+       int i=0;
+       int n = intervals.length;
+
+       while(i < n){
+            // Case 1: Current interval ends before newInterval starts (No overlap)
+            if(intervals[i][1] < newInterval[0]){
+                ans.add(intervals[i]);
+            }
+            // Case 2: Current interval starts after newInterval ends (No overlap, insert position found)
+            else if ( intervals[i][0] > newInterval[1]){
+                break;
+            }
+            else{
+                // Case 3: Overlapping intervals -> Merge into newInterval and erase current interval
+                newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
+                newInterval[1] = Math.max(intervals[i][1], newInterval[1]);
+            }
             i++;
-        }
-        // Case 2: Current interval starts after newInterval ends (No overlap, insert position found)
-        else if ( intervals[i][0] > newInterval[1]){
-            return insertAt(intervals,newInterval,i);
-        }
-        else{
-            // Case 3: Overlapping intervals -> Merge into newInterval and erase current interval
-            newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
-            newInterval[1] = Math.max(intervals[i][1], newInterval[1]);
-            intervals = earseAt(intervals,newInterval,i);
-        }
        }
-
-       return insertAt(intervals, newInterval, intervals.length);
-    }
-
-
-    public int[][] insertAt(int[][] intervals, int[] value,int pos){
-        int[][] res =  new int[intervals.length+1][2];
-
-        for(int i=0; i<pos; i++){
-            res[i] = intervals[i];
-        } 
-        res[pos] = value; 
-
-        for(int i = pos; i<intervals.length; i++){
-            res[i+1] = intervals[i];
-        }
-        return res;
-    }
-
-    public int[][] earseAt(int[][] intervals, int[] value,int pos){
-        int[][] res =  new int[intervals.length-1][2];
-
-        for(int i=0; i<pos; i++){
-            res[i] = intervals[i];
-        }
-        for(int i=pos+1; i<intervals.length; i++){
-            res[i-1] = intervals[i];
-        }
-        return res;
+       ans.add(newInterval);
+       //Add all remaining intervals that start AFTER newInterval ends 
+       while(i<intervals.length){
+        ans.add(intervals[i]);
+        i++;
+       }
+        
+       return ans.toArray( new int[ans.size()][]);
     }
 }

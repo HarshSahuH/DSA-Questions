e8841f1e84e2aaf9ec3211caa0f0cd06d66d4409
Harsh Sahu <121496378+HarshSahuH@users.noreply.github.com>
2026-08-23 11:34:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
@@ -1,34 +1,27 @@
 class Solution {
-    
-    public int solve(int w,int[]val, int[] wt, int n, int[][]dp){
-        
-        if(n==0 || w==0){
-            return 0;
-        }
-        if(dp[n][w] !=-1){
-            return dp[n][w];
-        }
-        if(wt[n-1] <= w){
-            dp[n][w] = Math.max(
-                    val[n-1] + solve(w-wt[n-1], val, wt, n-1,dp),
-                    solve(w, val, wt, n-1,dp));
-        }
-        else{
-            //( wt[n-1] > w)
-            dp[n][w] = solve(w,val,wt,n-1,dp);
-        }
-        return dp[n][w];
-    }
     public int knapsack(int W, int val[], int wt[]) {
-        int n = wt.length;
-        int rows = n+1;
-        int cols = W+1;
-        int[][] dp = new int[rows][cols];
+        int n = val.length;
+        int[][] dp = new int[n+1][W+1];
         
-        for(int[] row : dp){
-            Arrays.fill(row,-1);
+        for(int i=0; i<n+1;i++){
+            for(int j=0; j<W+1; j++){
+                if(i==0 || j==0){
+                    dp[i][j]=0;
+                }
+            }
         }
         
-        return solve(W,val,wt,n,dp);
+        for(int i=1; i<n+1; i++){
+            for(int j=1; j<W+1; j++){
+                if(wt[i-1] <= j){
+                    dp[i][j] = Math.max(val[i-1] + dp[i-1][j-wt[i-1]], dp[i-1][j]);
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        
+    return dp[n][W];    
     }
 }

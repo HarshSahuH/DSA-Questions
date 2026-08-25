684429672306621953797f4b44bf286b0f8203f4
Harsh Sahu <121496378+HarshSahuH@users.noreply.github.com>
2026-08-25 10:51:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sum Problem/subset-sum-problem.java
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.java
@@ -1,25 +1,35 @@
 class Solution {
-	public static boolean solve(int arr[], int sum, int n) {
+	public static boolean solve(int arr[], int sum, int n, Boolean[][] dp) {
+		// 1. Base Cases (Turant return karein)
 		if (sum == 0) {
-			return true;
+			return  true;
 		}
-		
 		if (n == 0) {
 			return false;
 		}
-		// choice diagram
-		// include kro
+		
+		// 2. Memoization Check (Hamesha top par hona chahiye)
+		if(dp[n][sum] != null){
+		    return dp[n][sum];
+		}
+		
+		// 3. Choice Diagram & Store in Table
 		if (arr[n - 1] <= sum) {
-			return solve(arr, sum-arr[n - 1], n-1) || solve(arr, sum, n-1);
+			dp[n][sum] = solve(arr, sum - arr[n - 1], n - 1,dp) || solve(arr, sum, n - 1,dp);
 		}
 		else {
-			return solve(arr, sum, n-1);
+			dp[n][sum] = solve(arr, sum, n - 1,dp);
 		}
+		
+		//at the end last cell return kr do jisme answer hai 
+		return dp[n][sum];
 	}
 	static boolean isSubsetSum(int arr[], int sum) {
 		// code here
 		int n = arr.length;
+		Boolean dp[][] = new Boolean[n+1][sum+1];
 		
-		return solve(arr, sum, n);
+		
+		return solve(arr, sum, n, dp);
 	}
 }

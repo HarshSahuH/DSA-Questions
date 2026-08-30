290fb302e1654c38f66b7b83c2cf0e53dc222cc5
Harsh Sahu <121496378+HarshSahuH@users.noreply.github.com>
2026-08-30 22:22:09 +0530

    Time: 3 ms (9.2%), Space: 46.4 MB (5.71%) - LeetHub

--- 4038-count-integers-appearing-in-a-single-block/4038-count-integers-appearing-in-a-single-block.java
+++ 4038-count-integers-appearing-in-a-single-block/4038-count-integers-appearing-in-a-single-block.java
@@ -1,21 +1,27 @@
+import java.util.HashMap;
+
 class Solution {
     public int countSpecialIntegers(int[] nums) {
-        int n = nums.length;
-        int count = 0;
         HashMap<Integer, Integer> map = new HashMap<>();
 
+        // Step 1: Map mein pehla element dalo (put)
         map.put(nums[0], 1);
+
+        // Step 2: Continuous blocks count karo
         for (int i = 1; i < nums.length; i++) {
             if (nums[i] != nums[i - 1]) {
                 map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
             }
         }
 
+        // Step 3: Block values par iterate karo (values)
+        int count = 0;
         for (int block : map.values()) {
             if (block == 1) {
                 count++;
             }
         }
+
         return count;
     }
 }

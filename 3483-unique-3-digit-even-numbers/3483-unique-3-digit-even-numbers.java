7c890ee4d15bec2525cbb9c1caff6c7321cd01ba
Harsh Sahu <121496378+HarshSahuH@users.noreply.github.com>
2026-09-11 16:49:24 +0530

    Time: 5 ms (82.8%), Space: 46.7 MB (20.53%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
@@ -1,42 +1,24 @@
 class Solution {
-    public boolean check(int unitPlace, int tensPlace, int hundredPlace, int[] count) {
-       count[unitPlace]--;
-       count[tensPlace]--;
-       count[hundredPlace]--;
-
-       boolean isValid = count[unitPlace] >=0 && count[tensPlace]>=0 && count[hundredPlace] >=0;
-
-       count[unitPlace]++;
-       count[tensPlace]++;
-       count[hundredPlace]++; 
-
-       return isValid;
-    }
-
     public int totalNumbers(int[] digits) {
-        int totalDistinctNumber = 0;
-        
-        // Digits 0-9 ki frequency store karne ke liye simple array
-        int[] count = new int[10];
-        for (int digit : digits) {
-            count[digit]++;
-        }
+        int n = digits.length;
+        Set<Integer> numList = new HashSet<>();
 
-        for (int i = 100; i <= 998; i+=2) {
-            int number = i;
+        for(int hundredPlace=0; hundredPlace<n; hundredPlace++){
+            for(int tensPlace=0; tensPlace<n; tensPlace++){
+                for(int unitPlace=0; unitPlace<n; unitPlace++){
 
-            int unitPlace = number % 10;
-            number = number / 10;
-
-            int tensPlace = number % 10;
-            number = number / 10;
-
-            int hundredPlace = number % 10;
-
-            if (check(unitPlace, tensPlace, hundredPlace, count)) {
-                totalDistinctNumber++;
+                    if(hundredPlace != tensPlace &&  tensPlace != unitPlace && unitPlace != hundredPlace){
+                        if(digits[unitPlace] %2 == 0){
+                            if(digits[hundredPlace] != 0){
+                                int number = 100*digits[hundredPlace] + 10*digits[tensPlace] + digits[unitPlace];
+                                numList.add(number);
+                            }
+                        }
+                    }
+                    
+                }
             }
         }
-        return totalDistinctNumber;
+        return numList.size();
     }
 }
